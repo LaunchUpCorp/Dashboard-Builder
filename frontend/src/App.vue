49e2b7b1dc62558a6d9773431699a8d5d65f8a2b
RuoxinHe <ruoxin.he@mail.utoronto.ca>
2023-05-03 13:15:50 -0400

    fixed the config file errors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="app">
-    <Title></Title>
+    <headerTitle></headerTitle>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import Title from './components/Title.vue'
+import { defineComponent } from 'vue'
+import headerTitle from './components/Title.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { Title }
+  components: { headerTitle }
 })
 </script>
 <style>
--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title">
+  <div class="headerTitle">
     <div class="relative">
       <img class="w-8 h-8 fixed top-5 left-0" :src="imageSrc" alt="header" />
       <h1 class="font-bold text-4xl fixed top-5 left-10">{{ title }}</h1>
@@ -12,7 +12,7 @@ import { defineComponent, ref } from 'vue'
 import imageSrc from '@/assets/title1.png'
 
 export default defineComponent({
-  name: 'title',
+  name: 'headerTitle',
   setup() {
     const title = ref('Launchup Dashboard Management')
     return { title, imageSrc }
