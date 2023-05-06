ee9c83028dc6bf2cdc608be4af564134e46bff1b
neli-rashkova <neli.rashkova73@gmail.com>
2023-05-06 13:03:44 -0400

    feat: imported font awesome icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 import App from './App.vue'
+import './index.css'
+library.add(fas, far)
 
-const app = createApp(App)
+const app = createApp(App).component('fa', FontAwesomeIcon)
 
 app.use(createPinia())
 
