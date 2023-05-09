98c955cc48529f0a936798e8d9b007ed6400aa49
Sung Min Sim <sungmin.sim@outlook.com>
2023-05-09 17:02:39 -0400

    refactor: remove Avatar component from App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Avatar from './components/Avatar/Avatar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Avatar
+  
   }
 })
 </script>
 
 <template>
     <div>
-      <Avatar imageUrl="http://placekitten.com/150/150" altText="Placeholder avatar" />
+      
     </div>
   </template>
