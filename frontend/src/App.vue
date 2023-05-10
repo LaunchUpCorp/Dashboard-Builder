11f0b4b64c886167ab37c150656ce846d77b7679
kelvin3127 <c.kelvin3127@gmail.com>
2023-05-10 14:42:38 -0400

    Fix: clean App.vue
    Remove code from Main file App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,63 +1,15 @@
 <script lang="ts">
     import { defineComponent } from 'vue';
-    import Dropdown from './components/Dropdown/Dropdown.vue'
-    import Test  from './components/Test/Test.vue';
 
     
     export default defineComponent({
         name: 'Main Component',
         components: {
-            Dropdown,
         },
     })
 
 </script>
 
 <template>
-    <div class="flex flex-row">
-        <div class="flex flex-col">
-            <Dropdown
-                theme = "default"
-            >
-                <span>Item 1</span>
-                <span>Item 2</span>
-                <span>Item 3</span>
-                <span>Item 4</span>
-            </Dropdown>
-            <Dropdown
-                theme = "light"
-            >
-                <span>Item 1</span>
-            </Dropdown>
-            <Dropdown
-                theme = "dark"
-            >
-                <span>Item 1</span>
-                <span>Item 2</span>
-                <span>Item 3</span>
-            </Dropdown>
-        </div>
-        <div class="flex flex-col">
-            <Dropdown
-                theme = "alert"
-            >
-                <span>Item 1</span>
-                <span>Item 2</span>
-            </Dropdown>
-            <Dropdown
-                theme = "confirm"
-            >
-                <span>Item 1</span>
-                <span>Item 2</span>
-                <span>Item 3</span>
-            </Dropdown>
-            <Dropdown
-                theme = "outline danger"
-            >
-                <span>Item 1</span>
-                <span>Item 2</span>
-                <span>Item 3</span>
-            </Dropdown>
-        </div>
-    </div>
+    <h1>Dashboard Builder</h1>
 </template>
