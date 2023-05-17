9f194480c419689c01060302240a3e77afe78298
kelvin3127 <c.kelvin3127@gmail.com>
2023-05-16 22:48:27 -0400

    feat: Line Chart
    Added ApexChart Library
    Added initial Code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import LineChartCard from './components/LineChartCard/LineChartCard.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-  
+    LineChartCard
   }
 })
 </script>
 
 <template>
-    <div>
-      
+    <div class="m-5" >
+      <LineChartCard />
     </div>
   </template>
